82f3b1141810cd287640eb81fb3bac2aec1ff6ce
Anderson Jorge <anderson.jorge33@gmail.com>
2021-09-06 21:16:59 -0300

    rotas com parametros

--- express/index.js
+++ express/index.js
@@ -6,16 +6,29 @@ app.get('/', (req, res) => {
   res.send('<h1>Bem vindo ao site</h1>')
 })
 
-app.get('/blog', (req, res) => {
-  res.send('<h1>Página Blog</h1>')
+app.get('/blog/:artigo?', (req, res) => {
+  let artigo = req.params.artigo
+
+  if(artigo){
+    res.send(`<h1>Nome do artigo é ${artigo}</h1>`)
+  } else {
+    res.send('<h1>Página Blog</h1>')
+  }
+
 })
 
 app.get('/perfil', (req, res) => {
-  res.send('<h1>Perfil</h1>')
+  res.send('<h1>Perfil do usuario</h1>')
 })
 
 app.get('/perfil/anderson', (req, res) => {
   res.send('<h1>Perfil do usuário anderson</h1>')
+})
+
+app.get('/ola/:nome/:empresa', (req, res) => {
+  let nome = req.params.nome
+  let empresa = req.params.empresa
+  res.send(`<h1>Oi ${nome} do ${empresa}</h1>`)
 })
 
 //criando um servidor com express
